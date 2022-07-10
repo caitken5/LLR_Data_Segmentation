201fad45be30e7d04030d53fc71aab3ef6799e02
sobriqu83 <caitken5@uwo.ca>
2022-07-10 21:29:17 +0100

    Added get_first_target_row (returns first index where the target to be acquired changes); added butterworth_filter function (takes in data and desired filtering frequency and applies forward and backwards second order filter to the data, assuming that sampling frequency is 100 Hz); added new imports. In main code, takes in file from source_folder and loads it into numpy array, identifies the first index of obtaining a new target. Obtains force data, applies 3 Hz filter to force signal (to smooth curve and remove higher frequency data such as tremor that might make it more difficult to determine the desired minimum in force), and applies Algorithm 1 with no variations, which simply takes the first minima of force after each new target occurs. Plots the force signal, the new target time as a vertical line, as well as the location of the time of the minimum force occurring after each new target is obtained.

--- Segmentation_header.py
+++ Segmentation_header.py
@@ -1,6 +1,26 @@
 # Here lies the constants and functions to be used in main.py of LLR_Data_Segmentation.
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
 
 data_header = ['Time', 'Des_X_Pos', 'Des_Y_Pos', 'X_Pos', 'Y_Pos', 'OptoForce_X', 'OptoForce_Y', 'OptoForce_Z',
                'OptoForce_Z_Torque', 'Theta_1', 'Theta_2', 'Fxy_Mag', 'Fxy_Angle', 'CorrForce_X', 'Corr_Force_Y',
                'Target_Num', 'X_Vel', 'Y_Vel', 'Vxy_Mag', 'Vxy_Angle', 'Dist_From_Target', 'Disp_Btw_Pts', 'Est_Vel',
                'To_From_Home', 'Num_Prev_Targets', 'Resistance']
+
+
+def get_first_target_row(data):
+    # Return the value of each of the first sets of data.
+    to_from_home = data[:, data_header.index('To_From_Home')]
+    diff = np.diff(to_from_home)
+    new_target = (np.asarray(np.where(diff != 0)).squeeze()).reshape((-1, 1))
+    new_target += 1
+    return new_target
+
+
+def butterworth_filter(data, freq):
+    # 2nd order 20 Hz Butterworth filter applied along the time series dimension of as many columns are passed to it.
+    # Will likely just be used to filter the velocity data, since a second order 20Hz Butterworth filter has already been applied to the force data.
+    sos = signal.butter(2, freq, 'lowpass', fs=100, output='sos')  # Filter cutoff of
+    filtered_data = signal.sosfiltfilt(sos, data, axis=0)
+    return filtered_data
--- main.py
+++ main.py
@@ -6,6 +6,14 @@
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+import Segmentation_header as h
+
+
+# TODO: Segment data using algorithm that takes the minimum of the applied force after a new target has been reached up
+#  until the next like segment for the next task.
 
 # Set up the file paths.
 source_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/3_LLR_DATA_CLEANING/" \
@@ -18,4 +26,46 @@ graph_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCES
              "GRAPHS"
 # The graph folder will be useful for plotting the newly segmented data, identifying where errors may be occurring,
 # and developing a plan of action for accounting for it.
+
+if __name__ == '__main__':
+    # Loop through numpy files that have been cleaned.
+    for file in os.listdir(source_folder):
+        if file.endswith('.npy'):
+            # Load the data.
+            source_file = source_folder + '/' + file
+            print(file)
+            data = np.load(source_file)
+            # Identify locations of change between target locations by using To_From_Home column. Add 1 to get correct
+            # index.
+            target_row = h.get_first_target_row(data)
+            # Get the time locations of the start of a new task.
+            t_targets = data[target_row, h.data_header.index('Time')]
+            t = data[:, h.data_header.index('Time')]
+            v = data[:, h.data_header.index('Vxy_Mag')]
+            d = data[:, h.data_header.index('Dist_From_Target')]
+            f = data[:, h.data_header.index('Fxy_Mag')]
+            # Apply filters to force and velocity data, but force is probably more likely to represent intention than velocity.
+            freq1 = 3
+            f1 = h.butterworth_filter(f, freq1)
+            # Identify, based on Algorithm 1 in notes.
+            first_min = []
+            my_min = (np.asarray(signal.argrelmin(f1)).flatten()).reshape((-1, 1))
+            min_time = t[my_min]
+            # Based on Algorithm 1, find the first instance of min_ind that is greater than each subsequent t_target.
+            for i in t_targets:
+                j2 = [j for j in min_time if j > i]
+                first_min.append(np.round(j2[0][0], 2))
+            # Plot what this looks like.
+            fig, ax = plt.subplots()
+            fig.set_size_inches(25, 8)
+            [ax.axvline(_t_targets, color='k') for _t_targets in t_targets]
+            # ax.plot(t, f, 'g-', label="Force Magnitude, no filter")
+            ax.plot(t, f1, 'b-', label="Force Magnitude, 3 Hz filter")
+            [ax.axvline(_my_min, color='g') for _my_min in first_min]
+            # ax.plot(t, d/500, 'b-', label="Distance from Target")
+            plt.legend()
+            plt.show()
+            plt.close()
+
+
 
